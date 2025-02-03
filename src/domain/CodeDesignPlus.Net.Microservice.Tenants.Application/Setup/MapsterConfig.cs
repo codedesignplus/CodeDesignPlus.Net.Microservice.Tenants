d1dcda7c39e9125b9972b20910b006c08528941e
CodeDesignPlus <codedesignsplus@outlook.com>
2025-02-02 22:33:57 -0500

    refactor: Simplify Mapster configuration for tenant commands by removing unused imports

--- src/domain/CodeDesignPlus.Net.Microservice.Tenants.Application/Setup/MapsterConfig.cs
+++ src/domain/CodeDesignPlus.Net.Microservice.Tenants.Application/Setup/MapsterConfig.cs
@@ -1,14 +1,13 @@
 ﻿using CodeDesignPlus.Microservice.Api.Dtos;
 using CodeDesignPlus.Net.Microservice.Tenants.Application.Tenant.Commands.CreateTenant;
 using CodeDesignPlus.Net.Microservice.Tenants.Application.Tenant.Commands.UpdateTenant;
-using CodeDesignPlus.Net.Microservice.Tenants.Domain.ValueObjects;
 
 namespace CodeDesignPlus.Net.Microservice.Tenants.Application.Setup;
 
 public static class MapsterConfigTenant
 {
-    public static void Configure() { 
-        
+    public static void Configure()
+    {
         //Tenant
         TypeAdapterConfig<CreateTenantDto, CreateTenantCommand>.NewConfig();
         TypeAdapterConfig<UpdateTenantDto, UpdateTenantCommand>.NewConfig();
