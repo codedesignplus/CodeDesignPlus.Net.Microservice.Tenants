be8260114bb369f0bff6b9628d1cec466739476c
Wilzon Liscano <wliscano@codedesignplus.com>
2025-09-05 18:30:19 -0500

    feat: add UpdateTenant and DeleteTenant RPC methods with corresponding request messages

--- src/entrypoints/CodeDesignPlus.Net.Microservice.Tenants.gRpc/Core/Mapster/MapsterConfig.cs
+++ src/entrypoints/CodeDesignPlus.Net.Microservice.Tenants.gRpc/Core/Mapster/MapsterConfig.cs
@@ -1,4 +1,5 @@
 ﻿using CodeDesignPlus.Net.Microservice.Tenants.Application.Tenant.Commands.CreateTenant;
+using CodeDesignPlus.Net.Microservice.Tenants.Application.Tenant.Commands.UpdateTenant;
 using NodaTime;
 using NodaTime.Serialization.Protobuf;
 using NodaTime.Text;
@@ -12,6 +13,35 @@ public static class MapsterConfig
         TypeAdapterConfig.GlobalSettings
             .NewConfig<CreateTenantRequest, CreateTenantCommand>()
             .ConstructUsing(src => new CreateTenantCommand(
+                Guid.Parse(src.Id),
+                src.Name,
+                Domain.ValueObjects.TypeDocument.Create(src.TypeDocument.Code, src.TypeDocument.Name),
+                src.NumbreDocument,
+                string.IsNullOrEmpty(src.Domain) ? null : new Uri(src.Domain),
+                src.Phone,
+                src.Email,
+                Domain.ValueObjects.Location.Create(
+                    Domain.ValueObjects.Country.Create(Guid.Parse(src.Location.Country.Id), src.Location.Country.Name, (ushort)src.Location.Country.Code, src.Location.Country.Timezone, Domain.ValueObjects.Currency.Create(Guid.Parse(src.Location.Country.Currency.Id), src.Location.Country.Currency.Name, src.Location.Country.Currency.Code, src.Location.Country.Currency.Symbol)),
+                    Domain.ValueObjects.State.Create(Guid.Parse(src.Location.State.Id), src.Location.State.Name, src.Location.State.Code),
+                    Domain.ValueObjects.City.Create(Guid.Parse(src.Location.City.Id), src.Location.City.Name, src.Location.City.Timezone),
+                    Domain.ValueObjects.Locality.Create(Guid.Parse(src.Location.Locality.Id), src.Location.Locality.Name),
+                    Domain.ValueObjects.Neighborhood.Create(Guid.Parse(src.Location.Neighborhood.Id), src.Location.Neighborhood.Name),
+                    src.Location.Address,
+                    src.Location.PostalCode
+                ),
+                Domain.ValueObjects.License.Create(
+                    Guid.Parse(src.License.Id),
+                    src.License.Name,
+                    InstantPattern.General.Parse(src.License.StartDate).Value,
+                    InstantPattern.General.Parse(src.License.EndDate).Value,
+                    src.License.Metadata.ToDictionary()
+                ),
+                src.IsActive
+            ));
+
+        TypeAdapterConfig.GlobalSettings
+            .NewConfig<UpdateTenantRequest, UpdateTenantCommand>()
+            .ConstructUsing(src => new UpdateTenantCommand(
                 Guid.Parse(src.Id),
                 src.Name,
                 Domain.ValueObjects.TypeDocument.Create(src.TypeDocument.Code, src.TypeDocument.Name),
--- src/entrypoints/CodeDesignPlus.Net.Microservice.Tenants.gRpc/Services/TenantService.cs
+++ src/entrypoints/CodeDesignPlus.Net.Microservice.Tenants.gRpc/Services/TenantService.cs
@@ -1,5 +1,7 @@
 using CodeDesignPlus.Net.Exceptions.Guards;
 using CodeDesignPlus.Net.Microservice.Tenants.Application.Tenant.Commands.CreateTenant;
+using CodeDesignPlus.Net.Microservice.Tenants.Application.Tenant.Commands.DeleteTenant;
+using CodeDesignPlus.Net.Microservice.Tenants.Application.Tenant.Commands.UpdateTenant;
 using CodeDesignPlus.Net.Microservice.Tenants.Application.Tenant.Queries.GetTenantById;
 using CodeDesignPlus.Net.Microservice.Tenants.Infrastructure;
 using Google.Protobuf.WellKnownTypes;
@@ -11,6 +13,26 @@ public class TenantService(IMediator mediator, IMapper mapper) : Tenant.TenantBa
     public override async Task<Empty> CreateTenant(CreateTenantRequest request, ServerCallContext context)
     {
         var command = mapper.Map<CreateTenantCommand>(request);
+
+        await mediator.Send(command, context.CancellationToken);
+
+        return new Empty();
+    }
+
+    public override async Task<Empty> UpdateTenant(UpdateTenantRequest request, ServerCallContext context)
+    {
+        var command = mapper.Map<UpdateTenantCommand>(request);
+
+        await mediator.Send(command, context.CancellationToken);
+
+        return new Empty();
+    }
+
+    public override async Task<Empty> DeleteTenant(DeleteTenantRequest request, ServerCallContext context)
+    {
+        DomainGuard.IsFalse(Guid.TryParse(request.Id, out var id), Errors.TenantIdIsInvalid);
+
+        var command = new DeleteTenantCommand(id);
 
         await mediator.Send(command, context.CancellationToken);
 
