a1ff74a6ed601ca67c1edb182d65e019697cbc34
Wilzon Camilo Liscano Galindo <wliscano@codedesignplus.com>
2026-06-17 23:03:34 -0500

    chore: update SDK packages and remove local baget feed
    - Updated CodeDesignPlus.Net.* packages
    - Removed local baget feed, using only public nuget.org

--- src/entrypoints/CodeDesignPlus.Net.Microservice.Tenants.gRpc/Program.cs
+++ src/entrypoints/CodeDesignPlus.Net.Microservice.Tenants.gRpc/Program.cs
@@ -58,7 +58,7 @@ if (!app.Environment.IsProduction())
 
 app.UseAuth();
 
-app.MapGrpcService<TenantService>().RequireAuthorization();
+app.MapGrpcService<TenantService>();
 
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
